98e9a5aebe65f55d5094cd43eb174603db7cc835
331uw13 <eeiuwie@gmail.com>
2025-09-27 05:28:34 +0300

    remove raylib build dir

--- raylib/build/raylib_modified/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eeiuwie/Dev/ambient3d/raylib/src
-# Build directory: /home/eeiuwie/Dev/ambient3d/raylib/build/raylib_modified
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
